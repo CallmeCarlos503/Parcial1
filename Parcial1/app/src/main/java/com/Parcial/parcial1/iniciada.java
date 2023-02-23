e400034291e77c6451f571c92965154ec2d10c2b
Carlos_Alberto_Sanchez <95440504+CallmeCarlos503@users.noreply.github.com>
2023-02-22 21:28:26 -0600

    finalizado

---
+++ Parcial1/app/src/main/java/com/Parcial/parcial1/iniciada.java
@@ -0,0 +1,14 @@
+package com.Parcial.parcial1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class iniciada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciada);
+    }
+}
